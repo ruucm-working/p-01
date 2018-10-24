f0ec5b545fa163ca111c831a282ae5973272912b
ruucm <ruucm.a@gmail.com>
2018-10-24 21:14:57 +0900

    change text

--- app/routes/HomePage.js
+++ app/routes/HomePage.js
@@ -4,7 +4,7 @@
  *
  */
 
-import React, { Component } from "react";
+import React, { Component } from 'react'
 
 class HomePage extends Component {
   // eslint-disable-line react/prefer-stateless-function
@@ -12,33 +12,31 @@ class HomePage extends Component {
   render() {
     return (
       <div>
-
-        <div style={{ color: "#6b6161" }}>
+        <div style={{ color: '#6b6161' }}>
           <div
             style={{
-              height: "100px",
-              backgroundColor: "#fcecd7",
-              borderRadius: "4px",
-              textAlign: "center",
-              paddingTop: "40px",
-              paddingBottom: "40px"
+              height: '100px',
+              backgroundColor: '#fcecd7',
+              borderRadius: '4px',
+              textAlign: 'center',
+              paddingTop: '40px',
+              paddingBottom: '40px',
             }}
           >
             <span
               style={{
-                lineHeight: "20px",
-                backgroundColor: "#b8e986",
-                color: "#4a90e2"
+                lineHeight: '20px',
+                backgroundColor: '#b8e986',
+                color: '#4a90e2',
               }}
             >
-              Hello Colorful World 🖍🖍
+              git 을 테스트 하고 있습니다
             </span>
           </div>
         </div>
-
       </div>
-    ); // eslint-disable-line
+    ) // eslint-disable-line
   }
 }
 
-export default HomePage;
+export default HomePage
